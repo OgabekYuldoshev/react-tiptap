dfb770236fd44bb9a8bd87c6b7ffbd926260ef35
OgabekYuldoshev <yuldashoff1@gmail.com>
2024-10-03 15:10:18 +0500

    refactor(deps): 🚑️ added react-node in web apps

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "react-note";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      App
+      <Button />
+    </div>
+  );
 };
 
 export default App;

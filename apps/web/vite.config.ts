6f4e140fe167e7458bd53ab9d60ac4d5ac5f8c96
OgabekYuldoshev <yuldashoff1@gmail.com>
2024-10-05 18:44:17 +0500

    feat: 🎨 added new deps

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,5 +1,5 @@
 // biome-ignore lint/style/useNodejsImportProtocol: <explanation>
-import { URL, fileURLToPath } from 'url'
+import { URL, fileURLToPath } from 'node:url'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 // https://vitejs.dev/config/

1d226dbcad4b2ca94aa9870712e1ad10d343c982
OgabekYuldoshev <yuldashoff1@gmail.com>
2024-10-07 10:31:14 +0500

    style(quality): 🎨 fixed code quality

--- package/src/components/Content.tsx
+++ package/src/components/Content.tsx
@@ -2,12 +2,12 @@ import { EditorContent } from "@tiptap/react";
 import { useNote } from "../context";
 
 const Content = () => {
-  const { editor } = useNote();
-  return (
-    <div className="rn-px-4 rn-py-2">
-      <EditorContent editor={editor} />
-    </div>
-  );
+	const { editor } = useNote();
+	return (
+		<div className="rn-px-4 rn-py-2">
+			<EditorContent editor={editor} />
+		</div>
+	);
 };
 
 export default Content;
--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,19 +1,18 @@
-// biome-ignore lint/style/useNodejsImportProtocol: <explanation>
-import { URL, fileURLToPath } from 'node:url'
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import { URL, fileURLToPath } from "node:url";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: fileURLToPath(new URL('./src', import.meta.url))
-      }
-    ]
-  },
-  server: {
-    port: 2121
-  }
-})
+	plugins: [react()],
+	resolve: {
+		alias: [
+			{
+				find: "@",
+				replacement: fileURLToPath(new URL("./src", import.meta.url)),
+			},
+		],
+	},
+	server: {
+		port: 2121,
+	},
+});
